b42891675b9df71bb32e0f1efd42e02ba591f748
Patrick Obrtal <patrick.obrtal@student.tuke.sk>
2022-06-22 13:53:00 +0200

    Added JSDocs to script.js

--- script.js
+++ script.js
@@ -1,20 +1,58 @@
 class PomodoroTimer {
+    /**
+     * @constructor
+     */
     constructor() {
+        /**
+         * @type {HTMLElement}
+         */
         this.workBtn = document.querySelector('#work-btn');
+        /**
+         * @type {HTMLElement}
+         */
         this.longBreakBtn = document.querySelector('#long-break-btn');
+        /**
+         * @type {HTMLElement}
+         */
         this.shortBreakBtn = document.querySelector('#short-break-btn');
+        /**
+         * @type {HTMLElement}
+         */
         this.timeDisplay = document.querySelector('#time-display');
+        /**
+         * @type {null | setInterval}
+         */
         this.timeout = null;
     }
     setEventListeners() {
+        /**
+         * @type {this}
+         */
         let iself = this;
+        /**
+         * @type {HTMLElement}
+         */
         this.workBtn.addEventListener("click", function () {iself.startTimer(25)});
+        /**
+         * @type {HTMLElement}
+         */
         this.longBreakBtn.addEventListener("click", function () {iself.startTimer(15)});
+        /**
+         * @type {HTMLElement}
+         */
         this.shortBreakBtn.addEventListener("click", function () {iself.startTimer(5)});
     }
+
+    /**
+     * @param seconds
+     * @returns {string|*}
+     */
     timePadding = (seconds) => {
         return seconds < 10 ? `0${seconds.toString()}` : seconds;
     }
+    /**
+     * @param seconds
+     */
     renderTime = (seconds) => {
         const minutesInText = Math.floor(seconds / 60);
         const actualSeconds = seconds % 60;
@@ -25,6 +63,9 @@ class PomodoroTimer {
         this.timeDisplay.textContent = time;
         document.title = `meow ${time}`;
     }
+    /**
+     * @param timeInMinutes
+     */
     startTimer = (timeInMinutes) => {
         let iself = this;
         clearInterval(this.timeout);
@@ -39,6 +80,9 @@ class PomodoroTimer {
     }
 }
 
+/**
+ * @type {PomodoroTimer}
+ */
 let Timer = new PomodoroTimer();
 Timer.setEventListeners();
 
